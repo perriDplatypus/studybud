ea5ecac25a6a3d756ebe9158403477e9dbdae4d3
abhay <abhaytyagi2513@gmail.com>
2023-07-31 23:11:48 +1000

    added dynamic urls

--- base/views.py
+++ base/views.py
@@ -2,9 +2,24 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
+rooms = [
+    {'id':1, 'name':'Lets learn python!'},
+    {'id':2, 'name':'Lets learn python!'},
+    {'id':3, 'name':'Lets learn python!'},
+    {'id':4, 'name':'Lets learn python!'},
+    {'id':5, 'name':'Lets learn python!'},
+]
+
+
 def home(request):
-    return render(request, 'home.html')
+    context = {'rooms': rooms}
+    return render(request, 'base/home.html', context)
 
 
-def room(request):
-    return render(request, 'room.html')
+def room(request, pk):
+    room = None
+    for i in rooms:
+        if i['id'] == int(pk):
+            room = i
+        context = {'room': room}
+    return render(request, 'base/room.html', context)
